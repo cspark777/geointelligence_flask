05afddff94515b4201b04f4da8cdb9ba253f8c21
Victor L <rhettb198501@hotmail.com>
2020-09-06 18:12:56 -0300

    fix

---
+++ credentials-ex.py
@@ -0,0 +1,11 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you
+API_KEY = ""
+API_SECRET_KEY = ""
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+
